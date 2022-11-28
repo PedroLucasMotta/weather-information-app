e78eca87d6a0b6be78011e75df6ff7d8caa06afd
Pedro Lucas <pedrolucasamotta@gmail.com>
2022-11-28 11:05:31 -0300

    chore: starting project

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+const APIKey = '2NXjmoUMB9v5NQFEYmKw0k8N5O4iqt8B'
+const baseUrl = 'http://dataservice.accuweather.com/'
+
+const getCityUrl = cityName => 
+    `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getWeatherUrl = cityKey => 
+    `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
+
+const fetchData = async url => {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obter os dados')
+        }
+
+        return response.json()
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityData = cityName => fetchData(getCityUrl(cityName))
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey))
